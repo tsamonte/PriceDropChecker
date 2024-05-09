f6e67232bb31e21822822163db33506c21045fed
Tristan Samonte <tsamonte23@gmail.com>
2024-05-09 15:26:24 -0700

    Implement scheduling

--- src/main.py
+++ src/main.py
@@ -2,14 +2,18 @@ import ProjectConfigs
 import manage_csv
 import price_check
 import email_handler
+import sys
+import schedule
+import time
 
-def main():
+def initialize_app():
     # Initialize project configurations from config.yaml file
     ProjectConfigs.initialize()
 
     # Attempt email login to ensure valid credentials were provided
     email_handler.test_login()
 
+def run_app():
     # Retrieve data from csv
     data = manage_csv.open_csv()
 
@@ -18,6 +22,25 @@ def main():
 
     # Update csv with the edited data
     manage_csv.update_csv(data)
+
+def main():
+    initialize_app()
+
+    # if cmd argument "persist" was passed, run price check logic every 2 hours
+    if len(sys.argv) >= 2 and sys.argv[1] == 'persist':
+        schedule.every(2).hours.do(run_app)
+
+        try:
+            print("Running application. Press CTRL+C to exit.")
+            while True:
+                schedule.run_pending()
+                time.sleep(1)
+        except (KeyboardInterrupt, SystemExit):
+            print("Program ended")
+
+    # if cmd argument "persist" wasn't passed, only run once
+    else:
+        run_app()
 
 
 if __name__ == '__main__':
